9600c853e86ac4e862391524dc5b65a7308e45d1
Nicole Citardi <oakapost@gmail.com>
2025-09-16 20:00:12 -0700

    doing a test, added print hi to TestCode.cs

--- Assets/TestCode.cs
+++ Assets/TestCode.cs
@@ -5,7 +5,8 @@ public class TestCode : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //test 
+        print("Hi");
     }
 
     // Update is called once per frame
